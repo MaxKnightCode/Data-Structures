321f2244cb5da3945d7f7cbd62983003815f2b3a
MaxKnightCode <46408990+MaxKnightCode@users.noreply.github.com>
2024-08-18 20:35:18 -0700

    Add files via upload

---
+++ MergeathanSort/Mergeathan/MergeSort.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Mergeathan
+{
+    internal class MergeSort<T> where T : IComparable<T>
+    {
+        public MergeSort()
+        {
+
+        }
+
+        public T[] Sort(T[] input)
+        {
+            if (input.Length < 2)
+            {
+                return input;
+            }
+
+            /*
+            //test
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.Write(input[i] + ", ");
+            }
+            Console.WriteLine();
+            */
+            
+
+            int midpt = (input.Length) / 2;
+
+            T[] leftArray = new T[midpt];
+            T[] rightArray = new T[input.Length - midpt];
+
+            for(int i = 0; i <  midpt; i++) 
+            {
+                leftArray[i] = input[i];
+            }
+            int x = 0;
+            for(int i = midpt; i < input.Length; i ++)
+            {
+                rightArray[x] = input[i];
+                x++;
+            }
+
+
+            T[] leftResult = Sort(leftArray);
+
+            T[] rightResult = Sort(rightArray);
+
+
+            return Merge(leftResult, rightResult);
+
+
+
+        }
+
+        
+        public T[] Merge(T[] left, T[] right)
+        {
+
+            T[] output = new T[left.Length + right.Length];
+
+            int leftCount = 0;
+            int rightCount = 0;
+
+            int outputCount = 0;
+
+            int RanOut = 0;
+
+            //left compared to the right is greater , 1
+            // left compared to right is less, -1
+
+            while (outputCount <= output.Length && RanOut == 0)
+            {
+                if (left[leftCount].CompareTo(right[rightCount]) < 0)
+                {
+                    output[outputCount] = left[leftCount];
+
+                    outputCount++;
+                    leftCount++;
+
+                    if (leftCount >= left.Length)
+                    {
+                        RanOut = -1;
+                    }
+                }
+
+                else if (left[leftCount].CompareTo(right[rightCount]) > 0)
+                {
+                    output[outputCount] = right[rightCount];
+
+                    outputCount++;
+                    rightCount++;
+
+                    if (rightCount >= right.Length)
+                    {
+                        RanOut = 1;
+                    }
+                }
+            }
+
+
+            if (RanOut == 1)
+            {
+                while (outputCount < output.Length)
+                {
+                    output[outputCount] = left[leftCount];
+
+                    outputCount++;
+                    leftCount++;
+                }
+            }
+            else if(RanOut == -1)
+            {
+                while (outputCount < output.Length)
+                {
+                    output[outputCount] = right[rightCount];
+
+                    outputCount++;
+                    rightCount++;
+                }
+            }
+
+
+
+
+            return output;
+
+
+        }
+
+
+
+    }
+}
---
+++ MergeathanSort/Mergeathan/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using Mergeathan;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+
+        MergeSort<string> test = new MergeSort<string>();
+
+
+
+        string[] array = new string[] { "cat" };
+        string[] test2 = test.Sort(array);
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"{test2[i]}, ");
+
+        }
+
+        
+
+    }
+}
+
